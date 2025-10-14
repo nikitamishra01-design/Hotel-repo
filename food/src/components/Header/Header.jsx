54979e82f539578bf4d82186008ce57571722fec
nikitamishra01-design <nikkibanarasi9559@gmail.com>
2025-10-14 23:43:37 +0530

    back

---
+++ food/src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./header.css";
+
+const Header = () => {
+  return (
+    <div  className='header'>
+      <div className='header-contents'>
+      <h2>order your favourite food here</h2>
+      <p>Order Delicious Food Online in Varanasi from tomato</p>
+      <button>view menu</button>
+
+      </div>
+    </div>
+  );
+}
+
+export default Header;
